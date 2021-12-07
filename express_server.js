328e3be5b81083d3f1e9266eb5b4455e35cb4096
Dmitri Yatsenko <dmitriyatsenko1995@gmail.com>
2021-12-07 21:08:17 +0000

    gener8 random string

--- express_server.js
+++ express_server.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
-
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+
+function generateRandomString() {
+    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let string = "";
+    for (i = 0; i < 6; i++) {
+        let x = characters.charAt(Math.floor(characters.length * Math.random()));
+        string += x;
+    }
+    return string;
+}
 
 const urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
@@ -16,6 +27,15 @@ app.get("/", (req, res) => {
 app.get("/urls", (req, res) => {
     const templateVars = { urls: urlDatabase };
     res.render("urls_index", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+    console.log(req.body); // Log the POST request body to the console
+    res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/urls/new", (req, res) => {
+    res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
